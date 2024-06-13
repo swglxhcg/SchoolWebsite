569a3b9363e2fd2ace1d3244ee06e1b22ad8cb67
ChZT <swglxhcg@163.com>
2024-06-13 11:29:09 +0800

    顶导航开始

--- js/t-navbar.js
+++ js/t-navbar.js
@@ -1,7 +1,11 @@
 // 调用函数
+
 navSubShow();
 searchInputBoxShow();
 fsnavShowSwitch();
+scrollWindow();
+
+
 // 调用函数END
 
 
@@ -55,3 +59,39 @@ function fsnavShowSwitch(){
         }
     });
 };
+
+function scrollWindow(){
+    $(window).scroll(function() {
+        $(window).scrollTop() > $(".t-header").height() ? $(".t-header").addClass("t-scrtop") : $(".t-header").removeClass("t-scrtop");
+    });
+};
+
+// function changeLogoArea(){
+//     $(".t-top .t-logo1").resize(function(){
+//         var t_logo_height=$(".t-logo1").height();
+//         var t_logo_width=$(".t-logo1").height();
+//         var t_w1=t_logo_width*0.38;
+//         var t_w2=t_logo_width*0.42;
+//         // var t_logo_coords1=`0, 0, $(t_w1), $(t_logo_height)`;
+//         var t_logo_coords1="0, 0, "+t_w1+", "+t_logo_height;
+//         // var t_logo_coords2=`$(t_w2), 0, $(t_logo_width), $(t_logo_height)`;
+//         var t_logo_coords2=t_w2+", 0, "+t_logo_width+", "+t_logo_height;
+//         $(".t-map-a-1").attr("coords", t_logo_coords1);
+//         $(".t-map-a-2").attr("coords", t_logo_coords2);
+//     });
+// };
+
+// function t_loaded_fun(){
+//     var t_logo_height=$(".t-logo1").height();
+//     var t_logo_width=$(".t-logo1").height();
+//     console.log(t_logo_height);
+//     console.log(t_logo_width);
+//     var t_w1=t_logo_width*0.38;
+//     var t_w2=t_logo_width*0.42;
+//     // var t_logo_coords1=`0, 0, $(t_w1), $(t_logo_height)`;
+//     var t_logo_coords1="0, 0, "+t_w1+", "+t_logo_height;
+//     // var t_logo_coords2=`$(t_w2), 0, $(t_logo_width), $(t_logo_height)`;
+//     var t_logo_coords2=t_w2+", 0, "+t_logo_width+", "+t_logo_height;
+//     $(".t-map-a-1").attr("coords", t_logo_coords1);
+//     $(".t-map-a-2").attr("coords", t_logo_coords2);
+// }
